cd58cc8b833eb7b6ef736ace037837ddb7853cbc
stasor <stasor12@gmail.com>
2018-12-10 23:38:47 +0200

    regex

---
+++ folder_4/input.js
@@ -0,0 +1,18 @@
+regExps = {
+"exercise_1": /[A-Z][a-z]+/,
+"exercise_2": /088[1-7]{7}/,
+"exercise_3": /[^0-1]{1,2}/,
+"exercise_4": /^[^0-9][A-Za-z0-9._]{2,13}$/,
+"exercise_5": /^[19].*[^1]$/,
+"exercise_6": /class=['"].+['"]/
+};
+cssSelectors = {
+"exercise_1": "css",
+"exercise_2": "css",
+"exercise_3": "css",
+"exercise_4": "css",
+"exercise_5": "css",
+"exercise_6": "css",
+"exercise_7": "css",
+"exercise_8": "css"
+};
